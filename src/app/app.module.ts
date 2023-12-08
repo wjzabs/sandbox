448d3c268188162ad63c3e08f387e95e219869d8
Walter J. Zielenski <wjz@absolution.com>
2023-12-08 17:30:56 -0500

    add modal in comp2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Comp1Component, Comp1PipeStartsWith } from './comp1/comp1.component';
-import { IgxAutocompleteModule, IgxDropDownModule, IgxInputGroupModule } from '@infragistics/igniteui-angular';
+import { IgxAutocompleteModule, IgxButtonModule, IgxDialogModule, IgxDropDownModule, IgxGridModule, IgxIconModule, IgxInputGroupModule, IgxRippleModule, IgxTabsModule } from '@infragistics/igniteui-angular';
 import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
 import { Comp2Component } from './comp2/comp2.component';
@@ -22,10 +22,17 @@ import { Comp2Component } from './comp2/comp2.component';
     HammerModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    IgxButtonModule,
+    IgxDialogModule,
     IgxAutocompleteModule,
     IgxDropDownModule,
     IgxInputGroupModule,
+    IgxRippleModule,
+	IgxIconModule,
+  IgxTabsModule,
+  IgxGridModule,
     FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/comp2/comp2.component.ts
+++ src/app/comp2/comp2.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GridColumnDataType } from '@infragistics/igniteui-angular';
 
 @Component({
   selector: 'app-comp2',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./comp2.component.scss']
 })
 export class Comp2Component {
+
+  public taskTitle = 'mow the lawn';
+
+btnClick(){
+  console.log('button clicked')
+}
+
+data: any[] = [
+  {issue_no: 1, issue_title: 'review modal interface'},
+  {issue_no: 2, issue_title: 'user feature request'},
+  {issue_no: 3, issue_title: 'hook up help system'},
+];
+
+datacolumns = [
+  { field: 'issue_no', header: '#'},
+  { field: 'issue_title', header: 'Issue'},
+];
+
+addTask() {
+
+  let issue_count = this.data.length
+this.data.push({issue_no: issue_count + 1, issue_title: this.taskTitle})
+}
+
+clearTask() {
+
+}
+ 
 
 }
